d804b6d9b87342c0b1e5a6e1f09d9e29da240aa3
Bo Skakke <bo@skakke.dk>
2015-06-08 12:47:15 +0200

    init

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
-<?php get_template_part('templates/page', 'header'); ?>
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
-
-<?php the_posts_navigation(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>BT.dk</title>
+	<link rel="stylesheet" href="/dist/styles/main.css">
+</head>
+<body>
+	Jeg er BT!
+</body>
+</html>
