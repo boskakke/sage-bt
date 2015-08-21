6426cb95165a18988dcf95b157b1c328fe185074
Bo Skakke <bo@skakke.dk>
2015-08-21 14:55:13 +0200

    lige nu live ticker

--- includes/liveticker-2.php
+++ includes/liveticker-2.php
@@ -4,18 +4,18 @@
 		</div>
 			<h1>LIGE NU <span>Jourhavende: <? dummy("text@author") ?></span></h1>
 			<ul>
-				<? while (dumb_luck("10")): ?>
+				<? while (dumb_luck("5")): ?>
 				<? if (dumb_luck("10%")): ?><li class="liveticker-live-sport"><? else: ?>
 					<? if (dumb_luck("10%")): ?><li class="liveticker-breaking"><? else: ?>
 						<? if (dumb_luck("10%")): ?><li class="liveticker-live"><? else: ?>
 							<? if (dumb_luck("10%")): ?><li class="liveticker-comingup"><? else: ?>
 	
 							<li><? endif ?><? endif ?><? endif ?><? endif ?>
-					<time><? dummy("text@time-ago-bt") ?></time>
+					<time><? dummy("text@time") ?></time>
 					
 					<h2><a href="article.php"><? dummy("text@headline") ?></a></h2>
-					<? if (dumb_luck("20%")): ?><figure><a href="article.php"><img src="<? dummy("image@300x,16:9") ?>"  width="" height="" alt="" /></a></figure><? endif ?>
-					<? if (dumb_luck("50%")): ?><p><? dummy("text@short-teaser") ?></p><? endif ?>
+					
+					
 				</li>
 				<? endwhile ?>
 			</ul>
